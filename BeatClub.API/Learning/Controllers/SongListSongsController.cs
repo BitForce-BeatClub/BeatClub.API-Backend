7c645797c2b5edc56867af1fd5e9eb150b0aa4a8
Josmarvs <siriusjomsar123@gmail.com>
2022-06-10 18:48:11 -0500

    songlistssongs controller added

---
+++ BeatClub.API/Learning/Controllers/SongListSongsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BeatClub.API.Learning.Domain.Models;
+using BeatClub.API.Learning.Domain.Services;
+using BeatClub.API.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeatClub.API.Learning.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/songlists/{songListId}/songs")]
+    public class SongListSongsController : ControllerBase
+    {
+        private readonly ISongService _songService;
+        private readonly IMapper _mapper;
+
+
+        public SongListSongsController(ISongService songService, IMapper mapper)
+        {
+            _songService = songService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<SongResource>> GetAllByPlayListId(int songListId)
+        {
+            var songs = await _songService.ListBySongListIdAsync(songListId);
+
+            var resources = _mapper.Map<IEnumerable<Song>, IEnumerable<SongResource>>(songs);
+
+            return resources;
+        }
+    }
+}
