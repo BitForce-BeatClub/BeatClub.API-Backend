cb254734c5a838716a575792e9773dc9ba8e9287
Psycho <u202019558@upc.edu.pe>
2022-06-11 05:37:36 -0500

    feat: creator added

---
+++ BeatClub.API/Learning/Domain/Models/Creator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace BeatClub.API.Learning.Domain.Models
+{
+    public class Creator
+    {
+        public int Id { get; set; }
+    
+        public string Name { get; set; }
+
+        public IList<Track> Tracks { get; set; } = new List<Track>();
+
+    }
+}
+
