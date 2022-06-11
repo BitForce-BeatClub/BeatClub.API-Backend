4681bed1730a5390c5c8eec24dc77080a721a065
Psycho <u202019558@upc.edu.pe>
2022-06-11 05:41:28 -0500

    feat: Added repositories for created classes.

---
+++ BeatClub.API/Learning/Domain/Repositories/ITrackRepository.cs
@@ -0,0 +1,18 @@
+﻿using BeatClub.API.Learning.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BeatClub.API.Learning.Domain.Repositories
+{
+    public interface ITrackRepository
+    {
+        Task<IEnumerable<Track>> ListAsync();
+        Task AddAsync(Track track);
+        Task<Track> FindByIdAsync(int trackId);
+        Task<Track> FindByContentAsync(string description);
+        Task<IEnumerable<Track>> FindByCreatorIdAsync(int creatorId);
+        void Update(Track track);
+        void Remove(Track track);
+    }
+}
+
---
+++ BeatClub.API/Learning/Persistence/Repositories/TrackRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeatClub.API.Learning.Domain.Models;
+using BeatClub.API.Learning.Domain.Repositories;
+using BeatClub.API.Learning.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeatClub.API.Learning.Persistence.Repositories
+{
+
+    public class TrackRepository : BaseRepository, ITrackRepository
+    {
+        public TrackRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Track>> ListAsync()
+        {
+            return await _context.Tracks
+                .Include(p => p.Creator)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Track track)
+        {
+            await _context.Tracks.AddAsync(track);
+        }
+
+        public async Task<Track> FindByIdAsync(int trackId)
+        {
+            return await _context.Tracks
+                .Include(p => p.Creator)
+                .FirstOrDefaultAsync(p => p.Id == trackId);
+
+        }
+
+        public async Task<Track> FindByContentAsync(string description)
+        {
+            return await _context.Tracks
+                .Include(p => p.Creator)
+                .FirstOrDefaultAsync(p => p.Description == description);
+
+        }
+
+        public async Task<IEnumerable<Track>> FindByCreatorIdAsync(int creatorId)
+        {
+            return await _context.Tracks
+                .Where(p => p.CreatorId == creatorId)
+                .Include(p=>p.Creator)
+                .ToListAsync();
+        }
+
+        public void Update(Track track)
+        {
+            _context.Tracks.Update(track);
+        }
+
+        public void Remove(Track track)
+        {
+            _context.Tracks.Remove(track);
+        }
+    }
+}
