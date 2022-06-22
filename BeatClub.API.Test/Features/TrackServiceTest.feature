36bc0146e6ffa8c36deed78a058cc311f0916ea2
Mathyu Cardozo <68485205+xMathyu@users.noreply.github.com>
2022-06-22 13:48:48 -0500

    Feature added

---
+++ BeatClub.API.Test/Features/TrackServiceTest.feature
@@ -0,0 +1,28 @@
+﻿Feature: TrackServiceTest
+	As a developer I want to add a new track through API
+	In order to make it available for applications 
+	
+	Background: 
+		Given the Endpoint https://localhost:7071/api/v1/tracks are available
+
+@track-adding
+		Scenario: Add track with unit title
+			When a Post Request is sent
+			| UserId | Title      | Privacy | Artist | Cover  | Source |
+			| 000003 | Shake that | Public  | Eminem | Sample | Sample |
+   			Then Response the status 200 is received 
+   			And A track resource is included in response body
+            | Id | UserId | Title      | Privacy | Artist | Cover  | Source |
+            | 1  | 000003 | Shake that | Public  | Eminem | Sample | Sample |
+            
+@track-adding
+		Scenario: Add track with existing title
+		Given A track is already stored
+		    | Id | UserId | Title      | Privacy | Artist | Cover  | Source |
+		    | 1  | 000003 | Shake that | Public  | Eminem | Sample | Sample |
+		When a Post Request is sent
+		    | UserId | Title 	       | Privacy | Artist	   | Cover  | Source |
+		    | 000004 | Congratulations | Public  | Post Malone | Sample | Sample |
+      	Then A response with the Status 400 is received 
+      	And An error message with value "track title already exist" is returned
+      	
---
+++ BeatClub.API.Test/Steps/TracksServiceStepDefinition.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BeatClub.API.Test.Steps;
+
+[Binding]
+public sealed class TracksServiceStepDefinition
+{
+    // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+    private readonly ScenarioContext _scenarioContext;
+
+    public TracksServiceStepDefinition(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given("the first number is (.*)")]
+    public void GivenTheFirstNumberIs(int number)
+    {
+        //TODO: implement arrange (precondition) logic
+        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        // To use the multiline text or the table argument of the scenario,
+        // additional string/Table parameters can be defined on the step definition
+        // method. 
+
+        _scenarioContext.Pending();
+    }
+
+    [Given("the second number is (.*)")]
+    public void GivenTheSecondNumberIs(int number)
+    {
+        //TODO: implement arrange (precondition) logic
+        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        // To use the multiline text or the table argument of the scenario,
+        // additional string/Table parameters can be defined on the step definition
+        // method. 
+
+        _scenarioContext.Pending();
+    }
+
+    [When("the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        //TODO: implement act (action) logic
+
+        _scenarioContext.Pending();
+    }
+
+    [Then("the result should be (.*)")]
+    public void ThenTheResultShouldBe(int result)
+    {
+        //TODO: implement assert (verification) logic
+
+        _scenarioContext.Pending();
+    }
+
+    [Given(@"the Endpoint https://localhost:(.*)/api/v(.*)/tracks are available")]
+    public void GivenTheEndpointHttpsLocalhostApiVTracksAreAvailable(int port, int version)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"a Post Request is sent")]
+    public void WhenAPostRequestIsSent(Table saveTrackResource)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"Response the status (.*) is received")]
+    public void ThenResponseIsReceivedWithStatus(int expectedStatus)
+    {
+        ScenarioContext.StepIsPending();
+    }
+}
