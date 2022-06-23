e1b502138e4c939d1d26b12d255d096d3e723fa6
Josmarvs <siriusjomsar123@gmail.com>
2022-06-22 23:40:19 -0500

    new controllers

---
+++ BeatClub.API/BeatClub/Controllers/UserPaymentsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BeatClub.API.BeatClub.Domain.Models;
+using BeatClub.API.BeatClub.Domain.Services;
+using BeatClub.API.BeatClub.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeatClub.API.BeatClub.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/users/{userId}/payments")]
+    public class UserPaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+        private readonly IMapper _mapper;
+
+        public UserPaymentsController(IPaymentService paymentService, IMapper mapper)
+        {
+            _paymentService = paymentService;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<PaymentResource>> GetAllByUserId(int userId)
+        {
+            var payments = await _paymentService.ListByUserIdAsync(userId);
+
+            var resources = _mapper.Map<IEnumerable<Payment>, IEnumerable<PaymentResource>>(payments);
+
+            return resources;
+        }
+        
+    }
+}
---
+++ BeatClub.API/BeatClub/Controllers/UserTracksController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BeatClub.API.BeatClub.Domain.Models;
+using BeatClub.API.BeatClub.Domain.Services;
+using BeatClub.API.BeatClub.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeatClub.API.BeatClub.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/users/{userId}/tracks")]
+    public class UserTracksController : ControllerBase
+    {
+        private readonly ITrackService _trackService;
+        private readonly IMapper _mapper;
+
+        public UserTracksController(ITrackService trackService, IMapper mapper)
+        {
+            _trackService = trackService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<TrackResource>> GetAllByUserId(int userId)
+        {
+            var tracks = await _trackService.ListByUserIdAsync(userId);
+
+            var resources = _mapper.Map<IEnumerable<Track>, IEnumerable<TrackResource>>(tracks);
+
+            return resources;
+
+        }
+
+
+    }
+}
